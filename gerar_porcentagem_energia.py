8a998d83048a869b35d152bf216eb5eeccc8648a
GrandeSasuke <158407213+GrandeSasuke@users.noreply.github.com>
2026-06-01 19:55:02 -0300

    git commit -m "fix: corrige erro de sintaxe no arquivo gerar_porcentagem_energia"

--- gerar_porcentagem_energia.py
+++ gerar_porcentagem_energia.py
@@ -6,11 +6,11 @@ situacao = ['NORMAL', 'ATENÇÃO', 'CRÍTICA', 'URGENTE']
 def gerar_valores_energia():
     definir_situacao = random.choices(situacao, weights=pesos, k=1)[0]
     if definir_situacao == 'NORMAL':
-        energia = random.randint(80, 100)
+        porcentagem_energia = random.randint(80, 100)
     elif definir_situacao == 'ATENÇÃO':
-        energia = random.randint(50, 79)
+        porcentagem_energia = random.randint(50, 79)
     elif definir_situacao == 'CRÍTICO':
-        energia = random.randint(30, 49)
+        porcentagem_energia = random.randint(30, 49)
     else:
-        energia = random.randint(0, 29)
-    return energia
+        porcentagem_energia = random.randint(0, 29)
+    return porcentagem_energia
