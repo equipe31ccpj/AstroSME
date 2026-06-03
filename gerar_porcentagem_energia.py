8aca556a5c5c300efd6eb632aa5639af02d7bd83
GrandeSasuke <158407213+GrandeSasuke@users.noreply.github.com>
2026-06-03 19:12:12 -0300

    fix: erro grámatical 'CRÍTICO' para 'CRÍTICA'

--- gerar_porcentagem_energia.py
+++ gerar_porcentagem_energia.py
@@ -9,7 +9,7 @@ def gerar_valores_energia():
         porcentagem_energia = random.randint(80, 100)
     elif definir_situacao == 'ATENÇÃO':
         porcentagem_energia = random.randint(50, 79)
-    elif definir_situacao == 'CRÍTICO':
+    elif definir_situacao == 'CRÍTICA':
         porcentagem_energia = random.randint(30, 49)
     else:
         porcentagem_energia = random.randint(0, 29)
